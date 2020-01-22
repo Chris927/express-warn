30476cda72b6a4cf7c02dce9c61732eb5b7fc806
Chris <chris@uber5.com>
2020-01-22 21:43:23 +0200

    Formatting (now including .ts files in prettier config)

--- src/warn.spec.ts
+++ src/warn.spec.ts
@@ -1,34 +1,34 @@
-import warn from './warn'
+import warn from "./warn";
 
-describe('warn', () => {
-  it('fails without options', () => {
+describe("warn", () => {
+  it("fails without options", () => {
     // @ts-ignore
-    expect(() => warn({})).toThrow(/missing/)
+    expect(() => warn({})).toThrow(/missing/);
     // @ts-ignore
-    expect(() => warn()).toThrow(/missing/)
-  })
+    expect(() => warn()).toThrow(/missing/);
+  });
   it('succeeds with just the "key" option', () => {
     // @ts-ignore
-    expect(typeof(warn({ keyFn: (r: Request) => r['bla'] }))).toBe('function')
-  })
-  it('logs', () => {
-    const logger = jest.fn()
-    const middleware = warn({ keyFn: (r:any) => r.bla, log: logger})
+    expect(typeof warn({ keyFn: (r: Request) => r["bla"] })).toBe("function");
+  });
+  it("logs", () => {
+    const logger = jest.fn();
+    const middleware = warn({ keyFn: (r: any) => r.bla, log: logger });
     // @ts-ignore
-    middleware(({ bla: 42 }), {}, jest.fn())
-    const { calls } = logger.mock
-    expect(calls.length).toBe(1)
-    expect(calls[0][0]).toBe(42)
-  })
-  it('throttles', () => {
-    const logger = jest.fn()
-    const middleware = warn({ keyFn: (r:any) => r.bla, log: logger})
+    middleware({ bla: 42 }, {}, jest.fn());
+    const { calls } = logger.mock;
+    expect(calls.length).toBe(1);
+    expect(calls[0][0]).toBe(42);
+  });
+  it("throttles", () => {
+    const logger = jest.fn();
+    const middleware = warn({ keyFn: (r: any) => r.bla, log: logger });
     for (let i = 0; i < 3; i++) {
       // @ts-ignore
-      middleware(({ bla: 42 }), {}, jest.fn())
+      middleware({ bla: 42 }, {}, jest.fn());
     }
-    const { calls } = logger.mock
-    expect(calls.length).toBe(1)
-    expect(calls[0][0]).toBe(42)
-  })
-})
+    const { calls } = logger.mock;
+    expect(calls.length).toBe(1);
+    expect(calls[0][0]).toBe(42);
+  });
+});
--- src/warn.ts
+++ src/warn.ts
@@ -1,27 +1,33 @@
-import { Request, Response, NextFunction } from 'express'
-import { throttle } from 'throttle-debounce'
+import { Request, Response, NextFunction } from "express";
+import { throttle } from "throttle-debounce";
 
 type Options = {
-  keyFn: (req: Request) => any,
-  mustWarnFn?: (v: any) => boolean,
-  log?: (...args: any[]) => void,
-  throttleMillis?: number
-}
+  keyFn: (req: Request) => any;
+  mustWarnFn?: (v: any) => boolean;
+  log?: (...args: any[]) => void;
+  throttleMillis?: number;
+};
 
 export default (options: Options) => {
-  if (!options|| !options.keyFn) {
-    throw new Error("'key' option is missing.")
+  if (!options || !options.keyFn) {
+    throw new Error("'key' option is missing.");
   }
-  const { keyFn} = options
-  const warnFn = options.mustWarnFn || function(v: any) { return v ? true : false }
-  const log = options.log || ((...args: any[]): void => console.log.apply(['WARN', ...args]))
-  const throttleMillis = options.throttleMillis || (10 /* seconds */ * 1000)
-  const throttledLogger = throttle(throttleMillis, log)
+  const { keyFn } = options;
+  const warnFn =
+    options.mustWarnFn ||
+    function(v: any) {
+      return v ? true : false;
+    };
+  const log =
+    options.log ||
+    ((...args: any[]): void => console.log.apply(["WARN", ...args]));
+  const throttleMillis = options.throttleMillis || 10 /* seconds */ * 1000;
+  const throttledLogger = throttle(throttleMillis, log);
   return function(req: Request, res: Response, next: NextFunction) {
-    const value = keyFn(req)
+    const value = keyFn(req);
     if (warnFn(value)) {
-      throttledLogger(value)
+      throttledLogger(value);
     }
-    next()
-  }
-}
+    next();
+  };
+};
